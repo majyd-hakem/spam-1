85d9113a62b4e5a0f173439054c6949916ff23fd
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-15 13:22:02 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,104 @@
+const Discord = require("discord.js");
+const myid = ['790233637580832788']
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const client3 = new Discord.Client();
+const client4 = new Discord.Client();
+const client5 = new Discord.Client();
+const client6 = new Discord.Client();
+
+
+client.login("NzE5NjE5NjIxOTcxNTU4NDY0.YHgPmg.8a_FgQU3ShAy0iAZtkFNWExRn5Y");
+client2.login("");
+client3.login("");
+client4.login("");
+client5.login("");
+client6.login("");
+
+//// By : Wolf
+client.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x <8000; x++) {
+        message.channel.send(`I'm not a Devloper I'm just a Student`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x <8000; x++) {
+        message.channel.send(`I'm not a Devloper I'm just a Student`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client3.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`I'm not a Devloper I'm just a Student`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client4.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`I'm not a Devloper I'm just a Student`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client5.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`I'm not a Devloper I'm just a Student`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client6.on('message', message => {
+if (message.content === '$spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 8000; x++) {
+        message.channel.send(`I'm not a Devloper I'm just a Student`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+
+///////By : WOLF
