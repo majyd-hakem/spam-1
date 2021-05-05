fd4c0f2479afff77957da3e42747528013e1860e
Majyd hakem <76665331+majyd-hakem@users.noreply.github.com>
2021-05-05 12:26:22 +0300

    Update wolf.js

--- wolf.js
+++ wolf.js
@@ -22,26 +22,26 @@ const wolf19 = new Discord.Client();
 const wolf20 = new Discord.Client();
 
 
-wolf.login("");
-wolf2.login("");
-wolf3.login("");
-wolf4.login("");
-wolf5.login("");
-wolf6.login("");
-wolf7.login("");
-wolf8.login("");
-wolf9.login("");
-wolf10.login("");
-wolf11.login("");
-wolf12.login("");
-wolf13.login("");
-wolf14.login("");
-wolf15.login("");
-wolf16.login("");
-wolf17.login("");
-wolf18.login("");
-wolf19.login("");
-wolf20.login("");
+wolf.login("Nzk5MzE4NjQyNDgzMzk2NjU4.YAB1fg.sWtMVv4ainWg8DSkx_lmrtQNCbY");
+wolf2.login("NzkwMzc3NDQ4MTA4OTgyMzcy.X9_uXA.ry4We68dhtqWlXtwDPF36zy8pf4");
+wolf3.login("Nzg2OTgxMjkwNDk0Mzk0NDI5.X9OULg.oXvzDTsJf2BFBpeAf_4B-DyWlps");
+wolf4.login("Nzg2NjQxNzM5MTQ2MTk5MTAw.X9JYSw.1pl3OV9kJRrtsbXS66pucNk7yYo");
+wolf5.login("Nzg2NjU4NTg4NDYxNzYwNTMz.X9OQZQ.6jGnb2eyirhK_zfyny6aeuQLhC4");
+wolf6.login("Nzg2NjQwNjM3NTA3NjY1OTky.X9JWng.YQOAHtHORFjJ7F-lPqKOBgXN0is");
+wolf7.login("Nzg2OTgxMjkwNDk0Mzk0NDI5.X9OcqA.pcIuW3oEfaxdyoF93HipAsISIhE");
+wolf8.login("Nzg3MjY2MzIzNDQ4MDcwMTQ0.X9SdyA.ebUJFq7d_Gc6XiLwn9w1Td0CyqU");
+wolf9.login("Nzg3MjY3NTExODgzMTM3MDM1.X9Sfhw.jwfsj6SHfMNTusMXaoD0g0vOwwg");
+wolf10.login("Nzg3MjY5NDY4NDA5NjkyMTYx.X9ShFw.Y4UdL1ZHvt2JE9kHUxY9kPJKYj4");
+wolf11.login("Nzg3MjcwNTE3NDcxNTc2MDg0.X9Sk1A.VX5iSiwFcUgl3g-gHW3suNPuBIQ");
+wolf12.login("Nzg3Mjc1MjI5MTMwMjYwNTAw.X9Sl5Q.qigHmKT3RGsHzi_WlLjV7yb7O-M");
+wolf13.login("Nzg3Mjc1ODgyNjE5NzMxOTY4.X9Sm7A.52yM9w2wmoBrTQD_le0Xz8OcoNg");
+wolf14.login("Nzg3Mjc3MDMxNDUxNzg3MjY0.X9SpTQ.7cm1mkudlYtB6-0_CpOFXSO5iLY");
+wolf15.login("Nzg3Mjc5Nzk2OTE2NzE1NTYx.X9Sqrw.WZZBhK3QAf58TFPCu8Eoo5mpYfo");
+wolf16.login("Nzg3Mjg0NDgzNDM4MTQ5NjYz.X9SuTg.CR-26Raqq12xyXt1vq3XEPaTj1E");
+wolf17.login("Nzg3Mjg2MDczMDIxNDMxODA4.X9Sv3A.ZsLqY61VHVfCOAvwWnitXV48enQ");
+wolf18.login("Nzg3Mjg2ODYxNDQ1NDY0MDk0.X9SweQ.B03wifBsaxVijYxnZp4-EJ6p_Rg");
+wolf19.login("Nzg3Mjg4MTgyNzU2MzQzODI4.X9Sx4g.9M9SLKMNDZmdyBNFz1z3WSrm0Mg");
+wolf20.login("Nzg3Mjg5NDMxMDY0MzEzODY3.X9S_Gw.np092qad5EkSoz3MHbVgFCKVzXE");
 
 //// By : Wolf
 wolf.on('message', message => {
